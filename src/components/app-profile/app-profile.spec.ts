bd2f3695e65278adc949c4a244cd6b3fddc1f300
Gerard van der Stelt <g.vanderstelt@skimgroup.com>
2021-02-15 22:33:36 +0100

    Initial Ionic

--- src/components/app-profile/app-profile.spec.ts
+++ src/components/app-profile/app-profile.spec.ts
@@ -8,39 +8,31 @@ describe('app-profile', () => {
         components: [AppProfile],
         html: '<app-profile></app-profile>',
       });
-      expect(rootInstance.normalize(undefined)).toEqual('');
-    });
-
-    it('returns a blank string if the name is null', async () => {
-      const { rootInstance } = await newSpecPage({
-        components: [AppProfile],
-        html: '<app-profile></app-profile>',
-      });
-      expect(rootInstance.normalize(null)).toEqual('');
+      expect(rootInstance.formattedName()).toEqual('');
     });
 
     it('capitalizes the first letter', async () => {
       const { rootInstance } = await newSpecPage({
         components: [AppProfile],
-        html: '<app-profile></app-profile>',
+        html: '<app-profile name="quincy"></app-profile>',
       });
-      expect(rootInstance.normalize('quincy')).toEqual('Quincy');
+      expect(rootInstance.formattedName()).toEqual('Quincy');
     });
 
     it('lower-cases the following letters', async () => {
       const { rootInstance } = await newSpecPage({
         components: [AppProfile],
-        html: '<app-profile></app-profile>',
+        html: '<app-profile name="JOSEPH"></app-profile>',
       });
-      expect(rootInstance.normalize('JOSEPH')).toEqual('Joseph');
+      expect(rootInstance.formattedName()).toEqual('Joseph');
     });
 
     it('handles single letter names', async () => {
       const { rootInstance } = await newSpecPage({
         components: [AppProfile],
-        html: '<app-profile></app-profile>',
+        html: '<app-profile name="Q"></app-profile>',
       });
-      expect(rootInstance.normalize('q')).toEqual('Q');
+      expect(rootInstance.formattedName()).toEqual('Q');
     });
   });
 });
--- src/global/app.ts
+++ src/global/app.ts
@@ -1,7 +1,8 @@
-export default async () => {
-  /**
-   * The code to be executed should be placed within a default function that is
-   * exported by the global script. Ensure all of the code in the global script
-   * is wrapped in the function() that is exported.
-   */
+import '@ionic/core';
+// import { setupConfig } from '@ionic/core';
+
+export default () => {
+  // setupConfig({
+  //   mode: 'ios'
+  // });
 };
